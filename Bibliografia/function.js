af0db096d5dbb02eb782f98b283982c589a7cfe5
Almontoya02 <42153887+Almontoya02@users.noreply.github.com>
2018-08-07 18:05:26 -0500

    Delete function.js

--- Bibliografia/function.js
+++
@@ -1,8 +0,0 @@
-function nuevo(){
-
-    var objectname = document.getElementById('txtName')
-    var txtName= document.getElementById('txtName').value
-  
-    console.log(txtName)
-    alert('hello world');
-}
